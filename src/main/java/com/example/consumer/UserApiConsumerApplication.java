6ab7a9373a026d53bca91544fb27477a6b41a29e
chanduk3 <132162196+chanduk3@users.noreply.github.com>
2024-03-07 14:14:15 -0600

    Update UserApiConsumerApplication.java

--- src/main/java/com/example/consumer/UserApiConsumerApplication.java
+++ src/main/java/com/example/consumer/UserApiConsumerApplication.java
@@ -1,7 +1,8 @@
 package com.example.consumer;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class UserApiConsumerApplication extends SpringBootServletInitializer {
